e2c5baf3f26d846c77d24e079d67a0888266fe92
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-03 01:11:08 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const makeChange = (c) => {
   // your name here
+	let quarter = 25;
+  let dime = 10;
+  let nickel = 5;
+  let penny = 1;
+  
+  const change ={
+      q : 0,
+      d : 0,
+      n : 0,
+      p : 0
+  }
+      while (c>= quarter)
+      {
+            change.q += 1; 
+            c = c-quarter;
+      }
+      
+      while (c < quarter && c >= dime)
+      {
+            change.d += 1; 
+            c = c-dime;
+      }
+      while (c < dime && c >= nickel)
+      {
+            change.n += 1; 
+            c = c-nickel;
+      }
+      while (c < nickel && c >= penny)
+      {
+            change.p += 1; 
+            c = c-penny;
+      }
+  return change
 };
 
 // Do not the change the code below
